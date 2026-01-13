5c0b9de25aff28f38f520b247196ed65d18f44e1
Bruno <brunohm.dev@gmail.com>
2026-01-12 22:08:43 -0300

    .

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,43 +1,17 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
+    public static void main(String[] args) { SpringApplication.run(ScreenmatchApplication.class, args); }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-        String apiKey = "3b046099";
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=dexter&season=1&apikey=" + apiKey);
-        ConverteDados conversor = new ConverteDados();
-        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-
-        List<DadosTemporada> temporadas = new ArrayList<>();
-
-        for (int i = 1; i < dados.totalTemporadas(); i++) {
-            json = consumoApi.obterDados("https://www.omdbapi.com/?t=dexter&season=" +
-                    i + "&apikey=" + apiKey);
-            DadosTemporada dadosTemp = conversor.obterDados(json, DadosTemporada.class);
-            temporadas.add(dadosTemp);
-        }
-
-        for (DadosTemporada temporada : temporadas) {
-            System.out.println(temporada);
-        }
-	}
+    @Override
+    public void run(String... args) {
+        Principal principal = new Principal();
+        principal.exibeMenu();
+    }
 }
--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -1,11 +1,22 @@
 package br.com.alura.screenmatch.principal;
 
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
+import br.com.alura.screenmatch.service.ConverteDados;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
     private ConsumoApi consumoApi;
+    private ConverteDados conversor =  new ConverteDados();
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=3b046099";
     private Scanner sc = new Scanner(System.in);
@@ -16,8 +27,47 @@ public class Principal {
         var url = ENDERECO + nomeSerie.replace(" ", "+") + API_KEY;
         consumoApi = new ConsumoApi();
         var json = consumoApi.obterDados(url);
+        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++) {
+            json = consumoApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +
+                    "&season=" +
+                    i + API_KEY);
+            DadosTemporada dadosTemp = conversor.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemp);
+        }
+
+        // temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        System.out.println("\n=== TOP 5 EPs MAIS BEM AVALIADOS POR SEASON ===\n\n" + nomeSerie.toUpperCase() + "\n");
+
+        /*dadosEpisodio.stream()
+                .filter(e -> !e.avaliacao().equals("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);*/
+
+        List<Episodio> episodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream()
+                        .map(d -> new Episodio(t.numero(), d))
+                ).collect(Collectors.toList());
+
+        System.out.println("Digite a partir de qual ano vc deseja ver: ");
+        int ano = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate dataEpisodios = LocalDate.of(ano, 1, 1);
+        DateTimeFormatter formatadorData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 
-
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataEpisodios))
+                .forEach(e -> System.out.println(
+                        "Temporada: " + e.getTemporada() +
+                                " Episódio: " + e.getTitulo() +
+                                " Data de lançamento: " + e.getDataLancamento().format(formatadorData)
+                ));
     }
 }
